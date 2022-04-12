b6637302f70c61fdd233b46ec77d388f40e2ed09
nehabht <ne.bhatti2@icloud.com>
2022-04-12 14:51:27 +0200

    add milestone1

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+/* Milestone 1
+Creiamo il nostro array di oggetti che rappresentano ciascun post. 
+Ogni post dovrà avere le informazioni necessarie per stampare la relativa card: */
+
+const posts = [
+    {
+        id: 1,
+        nome_autore: 'Anthony Bridgerton',
+        foto_autore: 'https://unsplash.it/300/300?image=',
+        data: 12-11-2022,
+        testo_post: "lorem ipsum dolor",
+        immagine: 'https://unsplash.it/300/300?image=',
+        numero_likes = 0
+    },
+    {
+        id: 2,
+        nome_autore: 'Kate Sharma',
+        foto_autore: 'https://unsplash.it/300/300?image=',
+        data: 12-11-2022,
+        testo_post: "lorem ipsum dolor",
+        immagine: 'https://unsplash.it/300/300?image=',
+        numero_likes = 1
+    },
+    {
+        id: 3,
+        nome_autore: 'Penelope',
+        foto_autore: 'https://unsplash.it/300/300?image=',
+        data: 12-11-2022,
+        testo_post: "lorem ipsum dolor",
+        immagine: 'https://unsplash.it/300/300?image=',
+        numero_likes = 1
+    },
+];
+
+
+/* Milestone 2
+Prendendo come riferimento il layout di esempio presente 
+nell'html, stampiamo i post del nostro feed. */
+
+
+
+
+
